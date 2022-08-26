0588704a01a32300ef1766dea0374efd4dcbb582
nejcrogelsek <nejcrogelsek0@gmail.com>
2022-08-26 16:34:35 +0200

    Test 3 - Object

--- src/pages/TestThree/TestThree.tsx
+++ src/pages/TestThree/TestThree.tsx
@@ -1,9 +1,65 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+
+interface Category {
+  id: number
+  name: string
+  sports: Sport[]
+}
+
+interface Sport {
+  id: number
+  name: string
+  category: number
+}
+
+const categories = [
+  { id: 0, name: 'Set sports' },
+  { id: 1, name: 'Competition sports' },
+  { id: 2, name: 'Other' },
+]
+
+const sports = [
+  { id: 2, name: 'Golf', category: 1 },
+  { id: 3, name: 'Formula 1', category: 1 },
+  { id: 0, name: 'Tennis', category: 0 },
+  { id: 5, name: 'Hockey', category: 2 },
+  { id: 1, name: 'Squash', category: 0 },
+  { id: 4, name: 'Soccer', category: 2 },
+]
 
 const TestThree: FC = () => {
+  const [generatedObject, setGeneratedObject] = useState<Category[]>()
+
+  const generateObject = () => {
+    const results: Category[] = []
+    for (let categoryIndex = 0; categoryIndex < categories.length; categoryIndex++) {
+      const validSports: Sport[] = []
+      const category = categories[categoryIndex]
+      for (let sportIndex = 0; sportIndex < sports.length; sportIndex++) {
+        const sport = sports[sportIndex]
+        if (category.id === sport.category) {
+          validSports.push(sport)
+        }
+      }
+      results.push({
+        id: category.id,
+        name: category.name,
+        sports: [...validSports],
+      })
+    }
+    setGeneratedObject(results)
+  }
+
+  useEffect(() => {
+    generateObject()
+  }, [])
+
   return (
     <div className="test-three">
-      <h1>TestThree</h1>
+      <h1>TEST 3 - Aggregated object </h1>
+      <div>
+        <pre>{JSON.stringify(generatedObject, undefined, 2)}</pre>
+      </div>
     </div>
   )
 }
