cedb8f40da3d653c2df123f55fd54495f591a096
nejcrogelsek <nejcrogelsek0@gmail.com>
2022-08-26 16:14:50 +0200

    Test 2 - VanillaJS

--- src/pages/TestTwo/TestTwo.tsx
+++ src/pages/TestTwo/TestTwo.tsx
@@ -1,9 +1,30 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 const TestTwo: FC = () => {
+  const [sentence, setSentence] = useState('today is a beautiful DAY here in trondheim. bye!')
+
+  const uppercaseEveryWord = () => {
+    const words = sentence.toLowerCase().split(' ')
+    for (let wordIndex = 0; wordIndex < words.length; wordIndex++) {
+      words[wordIndex] = words[wordIndex][0].toUpperCase() + words[wordIndex].substring(1)
+    }
+    const result = words.join(' ')
+    setSentence(result)
+  }
+
+  useEffect(() => {
+    uppercaseEveryWord()
+  }, [])
+
   return (
     <div className="test-two">
-      <h1>TestTwo</h1>
+      <h1>TEST 2 - Vanilla JS</h1>
+      <p>
+        <b>Old sentence:</b> today is a beautiful DAY here in trondheim. bye!
+      </p>
+      <p>
+        <b>New sentence:</b> {sentence}
+      </p>
     </div>
   )
 }
